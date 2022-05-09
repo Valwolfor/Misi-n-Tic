9691151f1fbf8bba43c7d2a6416c5b1436f59ee0
Valwolfor <diegofelipe.gallo@gmail.com>
2022-05-08 21:55:40 -0500

    Primer intento reto semana 2

---
+++ Reto/reto2Ciclo1.py
@@ -0,0 +1,56 @@
+#Entradas al parqué AVENTURAS EXTREMAS, una función que recibe dicionario para verificar :
+#Edad, primer_ingreso. 
+#El valor se puede ver variado según descuento 5% : X-Treme, 7% : carros chocones, 5% : sillas voladoras.  
+#Diccionario para datos del cliente.
+
+def cliente(informacion:dict)->dict:
+    #Func para verificar costo e ingreso a las atracciones.
+    #Verificación de apto según edad. 
+    if informacion["edad"] > 18:
+        apto = True
+        atraccion = "X-Treme"
+        if informacion["primer_ingreso"] == True:
+            total_boleta = 20000 - (20000 * 0.05)
+        else:
+            total_boleta = 20000
+    elif informacion["edad"] >= 15 and informacion["edad"] <= 18:
+        apto = True
+        atraccion = "Carros chocones"
+        if informacion["primer_ingreso"] == True:
+            total_boleta = 5000 - (5000 * 0.07)
+        else:
+            total_boleta = 5000
+    elif informacion["edad"] >= 7 and informacion["edad"] < 15:
+        apto = True
+        atraccion = "Sillas voladoras"
+        if informacion["primer_ingreso"] == True:
+            total_boleta = 10000 - (10000 * 0.05)
+        else:
+            total_boleta = 10000
+    else:
+        apto = False
+        primer_ingreso = False
+        atraccion = "N/A"
+        total_boleta ="N/A"
+    #Nuevo diccionario de salida.
+    salida = {
+        "nombre": informacion["nombre"],
+        "edad": informacion["edad"],
+        "atracción": atraccion,
+        "apto": apto,
+        "primer_ingreso": informacion["primer_ingreso"],
+        "total_boleta": total_boleta
+    }
+    return  salida
+
+#informacion["primer_ingreso"], #Solo en caso de 0, sino después del '#' == True.
+#Dejo el dicionario, pero creo que no se debe dejar.
+informacion = {
+    "id_cliente": 1, 
+    "nombre": "Diego",
+    "edad": 2,
+    "primer_ingreso": False         #Ojo aquí con el 0
+    }
+
+cliente(informacion)
+print(cliente(informacion))
--- _1.py
+++ _1.py
@@ -75,38 +75,6 @@
 # print(resultado(5))                 #necesita explicación!!!  
 
 
-#MóDULOS: Dividir un código, se llaman con "import" más el nombre de modulo, importante tenerlo documentado. 
-    #namespace, preguntar!!
-#Los modulos puede importar solo una función o elemento. 
-#2 from nombreModulo import elementoEspecificoAImportar
-#3 from nombreModulo import *           #El * asterisco indica importar todas las entidades del modulo
-    #aliasing(renombrar), para cambiar el nombre del módulo}
-    #4 import nombreModulo as alias
-        #si se desea cambiar la entidá del modulo
-        #4.1.1 from module import nombre as alias
-        #4.1.2 from module import n as a, m as b, o as c
-        #4.1.3 from math import pi as PI, sin as sine
-
-#Ejecutar para entender. Se colocan al final del último argumento de la func. 
-# print("Mi nombre es", "Python.", end=" ")
-# print("Mi nombre es", "Python.", end="\n")
-# print("Mi nombre es ", end="")
-# print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
-# print("Mi", "nombre", "es", "Monty", "Python.", sep="*")
-# print("Mi", "nombre", "es", sep="_", end="*")
-# print("Monty", "Python.", sep="*", end="*\n")
-# print("Fundamentos","Programación","en", sep="_", end="_")
-# print("Me gusta \"Monty Python\"")
-# print('Me gusta "Monty Python')
-# print(True > False)
-# print(True < False)
-# print('\"Estoy\" \n\"\"aprendiendo\"\" \n\"\"\"Python\"\"\'')
-
-# from random import *
-# print(randrange(1), end = ' ')
-# print(randrange(0, 20), end=' ')
-# print(randrange(0, 5, 1), end=' ')
-# print(randint(0, 2))
 
 ##Sensores alarma
     # sensor --> puerta abierta = True 
