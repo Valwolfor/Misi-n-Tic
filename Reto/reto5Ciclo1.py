351ebe3283e22549acdcaddd6f106ee5dbcbe896
Valwolfor <diegofelipe.gallo@gmail.com>
2022-06-04 18:18:33 -0500

    Todos los datos organizados hasta la semana 6. Ojo con el CRUD, ta bueno.

--- Reto/reto5Ciclo1.py
+++ Reto/reto5Ciclo1.py
@@ -1,9 +1,7 @@
 #Se necesita organizar una información por cierto criterios y tomando una muestra.
 
-from operator import index
 import pandas as pd
-pd.options.display.max_rows=10  #Especificación para configurar pandas en un maximo de rows
-
+pd.options.display.max_rows = 10  #Especificación para configurar pandas en un maximo de rows
 #ruta file csv
 rutaFileCsv = "https://raw.githubusercontent.com/luisguillermomolero/MisionTIC2022_2/master/Modulo1_Python_MisionTIC2022_Main/Semana_5/Reto/movies.csv"
 
@@ -11,10 +9,14 @@ rutaFileCsv = "https://raw.githubusercontent.com/luisguillermomolero/MisionTIC20
 def listaPeliculas(rutaFileCsv:str)->str :
     #Función para llamar archivo filtrando y contruir los elementos solicitados.
     # try:
-    datosCsv = pd.read_csv(rutaFileCsv, sep = ',',index_col = ["Country", "Language"], usecols = ["Country", "Language", "Gross Earnings"])
+    # datosCsv = pd.read_csv(rutaFileCsv, sep = ',', index_col = ["Country", "Language"], usecols = ["Country", "Language", "Gross Earnings"])
+    datosCsvSC = pd.read_csv(rutaFileCsv, sep = ',', usecols = ["Country", "Language", "Gross Earnings"])
     # except:
-    
-    print(datosCsv)
+        # print("Error al leer el archivo de datos.")
+    tablaDinamica = pd.pivot_table(datosCsvSC, index = ["Country", "Language"], columns = "Gross Earnings")
+    # tablaDinamica = pd.pivot_table(datosCsv)
+    # print(datosCsvSC.head(10))
+    # print(tablaDinamica)
 
 listaPeliculas(rutaFileCsv)
 print(listaPeliculas)
